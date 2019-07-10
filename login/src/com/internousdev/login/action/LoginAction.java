58bda30512745fd84d3caa1cfcff5399d8b63ffa
ShoheiMiyashiro123 <velonica0301.gmail>
2019-07-10 21:41:58 +0900

    0710

--- login/src/com/internousdev/login/action/LoginAction.java
+++ login/src/com/internousdev/login/action/LoginAction.java
@@ -7,23 +7,23 @@ import com.internousdev.login.dto.LoginDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport {
-	
+
 	private String name;
 	private String password;
-	
+
 	public String execute() throws SQLException{
 		String ret = ERROR;
-		
+
 		LoginDAO dao = new LoginDAO();
 		LoginDTO dto = new LoginDTO();
-		
+
 		dto = dao.select(name,password);
 		if(name.equals(dto.getName())){
-			if(name.equals(dto.getPassword())){
+			if(password.equals(dto.getPassword())){
 				ret=SUCCESS;
 			}
 		}
-		
+
 		return ret;
 	}
 
@@ -35,11 +35,11 @@ public class LoginAction extends ActionSupport {
 	}
 	public String getPassword(){
 		return password;
-	
+
 	}
 	public void setPassword(String password){
 		this.password=password;
-	
+
 	}
-	
+
 }
--- login/src/com/internousdev/login/dao/LoginDAO.java
+++ login/src/com/internousdev/login/dao/LoginDAO.java
@@ -13,29 +13,29 @@ public class LoginDAO {
 		LoginDTO dto=new LoginDTO();
 		DBConnector db=new DBConnector();
 		Connection con= db.getConnection();
-		
+
 		String sql="select * from user where user_name=? and password=?";
-		
+
 	try{
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setString(1, name);
 		ps.setString(2, password);
 		ResultSet rs=ps.executeQuery();
-		
+
 		if(rs.next()){
 			dto.setName(rs.getString("user_name"));
 			dto.setPassword(rs.getString("password"));
 		}
-		
+
 	}catch(SQLException e){
 		e.printStackTrace();
-		
+
 	}finally{
-		
+
 		con.close();
 	}
 		return dto;
-	
+
 	}
 
 }
